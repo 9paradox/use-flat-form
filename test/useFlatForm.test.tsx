d523fe83789e93ea26ab87abb9a753151fa522b1
Paradox <paradox.akshay@gmail.com>
2021-05-27 21:33:02 +0530

    added first useFlatForm test

--- test/useFlatForm.test.tsx
+++ test/useFlatForm.test.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import useFlatForm from './../src/';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -11,5 +12,17 @@ describe('it', () => {
     }
     const tree = Enzyme.shallow(<Component />);
     expect(tree.text()).toBe('Hey everyone!');
+  });
+
+  it('say hello using flat form', () => {
+    function Component() {
+      const initState = {
+        text: 'Hello useFlatForm',
+      };
+      const [myState] = useFlatForm(initState);
+      return <div>{myState.text}</div>;
+    }
+    const tree = Enzyme.shallow(<Component />);
+    expect(tree.text()).toBe('Hello useFlatForm');
   });
 });
